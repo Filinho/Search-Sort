dd82530c7c34acda26c1532eb55497d2d8e32491
Filinho <imsf@aluno.ifnmg.edu.br>
2023-03-01 17:42:53 -0300

    Update main.cpp

--- lista_encadeada/main.cpp
+++ lista_encadeada/main.cpp
@@ -1,8 +1,13 @@
 #include "DoubleLinkedList.h"
 
 int main(){
-    LinkedList * l = new LinkedList(40);
+    LinkedList * l = new LinkedList(10);
 
+    for(Node * current = l->StartHeadNode->Next; current != l->EndHeadNode;current = current->Next){
+       cout<<"Name: " << current->p.Name<< ", Key: "<< current->p.key << endl; 
+    }
+    cout <<"buble: " <<endl;
+    l->bubbleSort();
     for(Node * current = l->StartHeadNode->Next; current != l->EndHeadNode;current = current->Next){
        cout<<"Name: " << current->p.Name<< ", Key: "<< current->p.key << endl; 
     }
