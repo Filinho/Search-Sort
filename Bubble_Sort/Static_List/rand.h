c837d4f67be2b0bc28ee9770df4ad3c32e3a8e01
Filinho <imsf@aluno.ifnmg.edu.br>
2023-03-01 18:41:37 -0300

    Create rand.h

---
+++ Bubble_Sort/Static_List/rand.h
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+string  rand_name(const int & i){
+  int aux = i%30;
+  string names[30];
+  names[0] = "Flavio";
+  names[1] = "Jose";
+  names[2] = "Tadeu";
+  names[3] = "Alberto";
+  names[4] = "Breno";
+  names[5] = "Pedro";
+  names[6] = "Maicon";
+  names[7] = "Ivanderlei";
+  names[8] = "Alexandre";
+  names[9] = "Tatiane";
+  names[10] = "Marcos";
+  names[11] = "Laercio";
+  names[12] = "Vinicus";
+  names[13] = "PDF";
+  names[14] = "Gabriel";
+  names[15] = "Rafael";
+  names[16] = "Caribe";
+  names[17] = "Alberto M.";
+  names[18] = "Lucio";
+  names[19] = "Luciana";
+  names[20] = "Aline";
+  names[21] = "Josias";
+  names[22] = "Ana Luiza";
+  names[23] = "Florzinha";
+  names[24] = "LEAL";
+  names[25] = "Genivaldo";
+  names[26] = "Jubileu";
+  names[27] = "Knight of Ni";
+  names[28] = "Gandalfo";
+  names[29] = "Bilbo";
+
+  return names[aux];
+}
