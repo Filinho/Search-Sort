3075de5e9c99c5749017372079d7c335d038e590
Filinho <imsf@aluno.ifnmg.edu.br>
2023-03-01 18:33:19 -0300

    Update main.cpp

--- lista_encadeada/main.cpp
+++ lista_encadeada/main.cpp
@@ -2,15 +2,9 @@
 
 int main(){
     LinkedList * l = new LinkedList(10);
-
-    for(Node * current = l->StartHeadNode->Next; current != l->EndHeadNode;current = current->Next){
-       cout<<"Name: " << current->p.Name<< ", Key: "<< current->p.key << endl; 
-    }
-    cout <<"buble: " <<endl;
+    l->printList();
     l->bubbleSort();
-    for(Node * current = l->StartHeadNode->Next; current != l->EndHeadNode;current = current->Next){
-       cout<<"Name: " << current->p.Name<< ", Key: "<< current->p.key << endl; 
-    }
     cout << endl;
+    l->printList();
     delete(l);
 }
