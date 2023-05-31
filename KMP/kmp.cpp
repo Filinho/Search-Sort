3c208e6ebd26bb9e6132535324089eed9513542f
Filinho <imsf@aluno.ifnmg.edu.br>
2023-05-31 13:50:16 -0300

    KMP

--- KMP/kmp.cpp
+++ KMP/kmp.cpp
@@ -1,26 +1,27 @@
 #include<cstring>
 #include<iostream>
 #include<string>
+#include <string.h>
 
 using namespace std;
 
-int * computeFailureFunction(string pattern){
+    int * computeFailureFunction(string pattern){
     int patternLength = size(pattern);
-    int auxLength = 0;
+    int aux = 0;
 
-    int * FF = new int(patternLength);
-    FF[0] = 0;
+    int * FF = new int(patternLength); 
+    FF[0] = 0; 
     
     int i = 1;
     while(i <patternLength){
-        if(pattern[i] == pattern[auxLength]){
-            auxLength ++;
-            FF[i] = auxLength;
+        if(pattern[i] == pattern[aux]){
+            aux ++;
+            FF[i] = aux;
             i++;
         }
         else{
-            if(auxLength != 0){
-                auxLength = FF[auxLength -1];
+            if(aux != 0){
+                aux = FF[aux -1];
             }
             else{
                 FF[i] = 0;
@@ -32,7 +33,7 @@ int * computeFailureFunction(string pattern){
 }
 
 int * KMP(string pattern, string txt){
-    int * foundArray = new int(1000);
+    int * foundArray = new int(size(txt)+1);
     foundArray[0] = 0;
     int patternLength = size(pattern);
     int txtLength = size(txt);
@@ -61,10 +62,41 @@ int * KMP(string pattern, string txt){
     }
     return foundArray;
 }
-int main(){
-    int * found = KMP("aba","aabababaabcdeab");
-    for(int i = 1; i <= found[0]; i++){
-        cout << found[i] << endl;
+
+void printtext(int * found, char * txt,int size_txt,  int sz_pattern){
+    
+    int buff_index = 1;
+    int aux = -1;
+     
+    for(int i = 0; i<= size_txt ;i++){
+        if(i == found[buff_index]){
+            aux = i + sz_pattern;
+            buff_index++;
+        }
+        if( i<aux) cout<< "\e[31;10m" << txt[i] << "\e[m";
+        
+        else cout<< "\e[64;10m" << txt[i] << "\e[m";
     }
+
+    cout<<endl<<endl;
+}
+int main(){
+
+    string pattern,txt;
+    cin >> pattern;
+    cin >>txt;
+    
+    int * found = KMP(pattern,txt);
+    cout<< "found adresses : ";
+    for(int i = 1; i <= found[0]; i++){
+        if(i!=1) cout << ", ";
+        cout<<found[i];
+    }
+    
+
+    char *aux = &txt[0];
+    cout << ";"<< endl<< "pattern : " << pattern<<endl;
+    printtext(found,aux,size(txt),size(pattern));
+    delete(found);
     
 }
